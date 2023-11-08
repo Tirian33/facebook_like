f4d33acbdff7a2795cb75ee1abea64ebd78034a4
Alexander Way <asw241@mmstate.edu>
2023-11-08 14:18:31 -0600

    Just in case.

--- pst_rep_rea_bp.py
+++ pst_rep_rea_bp.py
@@ -34,6 +34,9 @@ def softDeleteObjects(listing) -> None:
 @pst_rep_rea_bp.route('/api/post', methods=['POST'])
 @jwt_required()
 def makePost():
+
+    print(request.form.get('textContent'))
+
     if ((request.form.get('textContent') is None and request.form.get('sharedPostId')is None) or request.form.get('postedOnID') is None):
         abort(400, "Invalid request recieved.")
 
@@ -53,7 +56,6 @@ def makePost():
     # If there is only one image attachment...
     if len(request.files.getlist('pic')) == 1 and request.files['pic'].mimetype != 'application/octet-stream':
 
-        
         # Retrieve the image object
         pic = request.files['pic']
         
